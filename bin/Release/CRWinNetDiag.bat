8acf2784752f61e85cacff42912d9cd95d605fd6
ChristianRe <reusch.christian@t-online.de>
2015-05-18 18:51:47 +0200

    Updated Version 0.241

---
+++ bin/Release/CRWinNetDiag.bat
@@ -0,0 +1,49 @@
+ chcp 1252
+rem chcp 858
+rem ReadMe:
+rem CRWinNetDiag.exe Version 0.2.4
+rem Beta Software
+rem CRWinNetDiag; The Tool collects TCP/IP Information for futher Analysis
+rem Copyright (C) 2015  Christian Reusch
+
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http:rem www.gnu.org/licenses/>.
+
+
+
+rem Diese Datei ist Teil von CRWinNetDiag.
+
+rem CRWinNetDiag ist Freie Software: Sie können es unter den Bedingungen
+rem der GNU General Public License, wie von der Free Software Foundation,
+rem Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+rem veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+rem CRWinNetDiag wird in der Hoffnung, dass es nützlich sein wird, aber
+rem OHNE JEDE GEWAEHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+rem Gewährleistung der MARKTFAEHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+rem Siehe die GNU General Public License für weitere Details.
+
+
+rem Dieses Programm befindet sich zur Zeit noch in der Beta Test Phase.
+
+rem Eine Benutzung ist nur zu Testzwecken zugelassen.
+rem Bei dem Einsatz solcher noch nicht zum Vertrieb freigegebener Software können 
+rem Fehlfunktionen und sogar Datenverluste auftreten. 
+
+
+
+
+CRWinNetDiag.exe > output.txt
+notepad output.txt
+
+pause
