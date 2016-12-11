02a3830a56a4a3d43ce3eb77544b33e934a82092
ChristianRe <ChristianRe@users.noreply.github.com>
2016-12-11 16:12:14 +0100

    Version 0.32 - Public Beta Test GPLv3

--- bin/Release/CRWinNetDiag.bat
+++ bin/Release/CRWinNetDiag.bat
@@ -1,19 +1,25 @@
  chcp 1252
 rem chcp 858
 rem ReadMe:
-rem WinNetDiag.exe Version 0.2.3
+rem WinNetDiag.exe Version 0.3.2
 rem Beta Software
 rem 
+rem This program is still in the beta testing phase
 rem Dieses Programm befindet sich zur Zeit noch in der Beta Test Phase.
 
+rem A usage is permitted only for test purposes
+rem At the usage of beta software wrong functions and data loss can occur
+rem 
 rem Eine Benutzung ist nur zu Testzwecken zugelassen.
 rem Bei dem Einsatz solcher noch nicht zum Vertrieb freigegebener Software können 
 rem Fehlfunktionen und sogar Datenverluste auftreten. 
 rem 
 
-rem Dieses Batch File startet das in der Public Beta Test Phase befindliche Programm WinNetDiag.exe.
+rem This Batch starts the public beta sofztware CRWinNetDiag.exe  With this programm
+rem it is possible to read the actual network configuration of windows host from all Windwos systems without the usage of privilege rights
+ 
+rem Dieses Batch File startet das in der Public Beta Test Phase befindliche Programm CRWinNetDiag.exe.
 rem Mit Hilfe dieses Programms ist es möglich die aktuelle Windowskonfiguration bezüglich der Netzwerkeinstellungen auszulesen
-rem Es empfiehlt sich die Standardausgabe mit Hilfe des [">" Operators]in eine Datei für die weitere Analyse umzuleiten
 
 type Readme.txt
 
